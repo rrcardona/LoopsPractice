652cd4a38fe94f7a999fe3729b8ca5e7019825fa
rrcardona <cardonas05@yahoo.com>
2017-08-29 20:43:54 -0400

    Worked on loops

--- LoopsPractice/Program.cs
+++ LoopsPractice/Program.cs
@@ -38,19 +38,136 @@ namespace LoopsPractice
             //Your lucky number is: 4
             //Your lucky number is: 19
             //Your lucky number is: 7
+            //string[] favFoods = new string[3];
+            //Console.WriteLine("What is your favorite food?");
+            //favFoods[0] = Console.ReadLine();
+            //Console.WriteLine("What is your second favorite food?");
+            //favFoods[1] = Console.ReadLine();
+            //Console.WriteLine("What is your third favorite food?");
+            //favFoods[2] = Console.ReadLine();
+            //foreach(string food in favFoods)
+            //{
+            //    Console.WriteLine(food);
+            //}
 
-            string[] favFoods = new string[3];
-            foreach (string food in favFoods) ;
-            Console.WriteLine("What is your fav food?");
-            Console.ReadLine();
-            Console.WriteLine("What is your second favorite food?");
-            Console.ReadLine();
-            Console.WriteLine("What is your third favorite food?");
-            Console.ReadLine();     
+            //int[] luckyNumbers = { 2, 4, 25 };
+            //foreach(int number in luckyNumbers)
+            //{
+            //    Console.WriteLine("Your lucky number is: " +number);
+            //}
+
+
+            //string[] Names = { "Jordan", "Max", "Peter" };
+            //for(int i = 0; i<Names.Length; i++)
+            //{
+            //    Names[i] = Names[i].ToLower();
+            //    Console.WriteLine(Names[i]);
+            //}
+
+            //for (int counter = 1; counter <= 10; counter+=2)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //string words = "I like cake";
+            //foreach(char letter in words)
+            //{
+            //    Console.WriteLine(letter);
+            //}
+
+            //string greetings = "My name is Ricardo";
+            //string[] words = greetings.Split();
+            //for(int i =0; i<words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //    if(words[i] =="Ricardo")
+            //    {
+            //        words[i] = "Max";
+            //    }
+            //   
+            //}
+            //Console.WriteLine(words[words.Length - 1]);
+
+
+            //Create an array called days with the elements monday tuesday etc...
+            //Use a for loop to print the elements to the console.
+
+            //Create an int array size 25 put the numbers 0 to 25 in the array and print those numbers to the console
+
+            //Start with the string "Once upon a time" and create an array called storyWords using
+            //the split method. Reverse the order of the elements in the array and using a for loop,
+            //print each word
+
+            //string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            //for (int i = 0; i < days.Length; i++)
+            //{
+            //    Console.WriteLine(days[i]);
+            //}
+
+            //int[]numbers = new int [26];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = i;
+            //    Console.WriteLine(i);
+
+            //}
+            //Console.WriteLine(numbers[numbers.Length-1]);
+
+            //string story = "Once upon a time";
+            //string[] storyWords = story.Split();
+            //for (int i = 0; i < storyWords.Length; i++)
+            //{
+            //    Console.WriteLine(storyWords[i]);
+            //}
+            //Array.Reverse(storyWords);
+            //for (int i = 0; i < storyWords.Length; i++)
+            //{
+            //    Console.WriteLine(storyWords[i]);
+            //}
+
+            // ***while loops***
+
+            //Console.WriteLine("Do you want to play a game?");
+            //string playAgain = Console.ReadLine();
+            //while(playAgain == "Yes")
+            //{
+            //    Console.WriteLine("It's a rematch");
+            //    Console.WriteLine("Do you want to play again?");
+            //    playAgain = Console.ReadLine();
+            //}
+
+            //Console.WriteLine("What is your favorite ROYGBIV color?");
+            //string userColor = Console.ReadLine();
+            ////bool isRoygbivColor = false;
+            //while (userColor !="red")
+            //{
+            //    Console.WriteLine("Sorry. That is not the correct color. Please try again");
+            //    userColor = Console.ReadLine();
+            //}
 
 
 
-            
+            // ***do while loops***
+
+            //string playAgain;
+            //do
+            //{
+            //    Console.WriteLine("Do you want to build a snowman?");
+            //    Console.WriteLine("That was fun!");
+            //    Console.WriteLine("Do you want to build another snowman?");
+            //    playAgain = Console.ReadLine();
+            //}
+            //while (playAgain == "yes");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 5)
+                    {
+                    break;
+                    }
+            }
+
 
 
 
